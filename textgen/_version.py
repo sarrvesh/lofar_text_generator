2cec280e9ed9883af4df7ca98428f8bd649702e0
sarrvesh <sarrvesh.ss@gmail.com>
2017-12-08 11:17:27 +0800

    moved to version 0.1

--- textgen/_version.py
+++ textgen/_version.py
@@ -4,10 +4,11 @@ This module stores the version number and the changelog
 """
 
 # Version number
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 # Changelog
 def changelog():
     """
     Changelog:
+    0.1.0    Code supports all imaging modes for both LBA and HBA.
     """
